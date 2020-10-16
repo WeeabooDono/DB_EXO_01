2240efb421ee434ce8449a400fcf98de69bd2444
Merlakre <corentin.beuchet@hotmail.fr>
2020-10-17 01:04:15 +0200

    correction

--- inc/ticket/ticketTools.php
+++ inc/ticket/ticketTools.php
@@ -2,56 +2,9 @@
 require_once('../../model/ticket.php');
 require_once( '../../inc/fct_utiles.php');
 
-function get($id){
-    $ticket = new ticket();
+function get($id){}
 
-    // open connection
-    $db = bdd();
-
-    // create an SQL request
-    $rqt = "SELECT * FROM `ticket` WHERE `idTicket` = $id";
-
-    // ask for an answer
-    $res = $db->query($rqt);
-
-    // for each rows of the request (here only one since $id is a primary key)
-    while ($val = $res->fetch(PDO::FETCH_ASSOC)) {
-        $ticket->setIdTicket($val['idTicket']);
-        $ticket->setDate($val['date']);
-        $ticket->setTechnicien($val['Technicien_idTechnicien']);
-    }
-    // close connection
-    $db = null;
-
-    return $ticket;
-}
-
-function getAll(){
-    $tickets = [];
-
-    // open connection
-    $db = bdd();
-
-    // create an SQL request
-    $rqt = "SELECT * FROM `ticket`";
-
-    // ask for an answer
-    $res = $db->query($rqt);
-
-    // for each rows of the request (here only one since $id is a primary key)
-    while ($val = $res->fetch(PDO::FETCH_ASSOC)) {
-        $ticket = new ticket();
-        $ticket->setIdTicket($val['idTicket']);
-        $ticket->setDate($val['date']);
-        $ticket->setTechnicien($val['Technicien_idTechnicien']);
-        $tickets[] = $ticket;
-    }
-
-    // close connection
-    $db = null;
-
-    return $tickets;
-}
+function getAll(){}
 
 function update($ticket){}
 
