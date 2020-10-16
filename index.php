dc2945b2541bbf45b28ea1879e5a14c97e9d6991
Merlakre <corentin.beuchet@hotmail.fr>
2020-10-16 22:06:15 +0200

    ajoute du schéma de la BDD

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/styles.css" />
-        <title>Choix</title>
-    </head>
-    <body>
-    <h1>Choisir un exercice</h1>
-    <ul>
-        <ol><a href="./tests/preparation_1.php">Préparation 1</a></ol>
-        <ol><a href="./tests/preparation_2.php">Préparation 2</a></ol>
-    </ul>
-    <pre>Passer à la suite de l'exercice</pre>
-    <a href="./home.php">Exercice</a>
-    </body>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css" />
+    <title>Choix</title>
+</head>
+
+<body>
+    <h1>Choisir un exercice de préparation</h1>
+
+    <h2>Commencer la <a href="./tests/preparation_1.php">préparation 1</a>.</h2>
+    <h2>Commencer la <a href="./tests/preparation_2.php">préparation 2</a>.</h2>
+
+    <h1>L'exercice</h1>
+    <h2>Débuter l'<a href="./home.php">exercice</a>.</h2>
+    <h2>Schéma de la base de donnée</h2>
+    <img src="./img/db_exercice.png" />
+</body>
+
 </html>
